9be044c4bb1e24316635f07bf4f4320c2426ff09
AlanHustler <110681275+AlanHustler@users.noreply.github.com>
2022-11-10 14:16:25 +0600

    Add files via upload

---
+++ web-layout/js/app.js
@@ -0,0 +1,22 @@
+// const openPage = document.querySelector(".english");
+// openPage.addEventListener("click", openPageFunc);
+
+// function openPageFunc() {
+//     window.open("index.html");
+// }
+
+// const openPageIelts = document.querySelector(".ielts");
+// openPageIelts.addEventListener("click", openPageFunc1);
+
+// function openPageFunc1() {
+//     window.open("index-lab.html");
+// }
+
+const openPageProg = document.querySelector(".prog");
+openPageProg.addEventListener("click", openPageFunc2);
+
+function openPageFunc2() {
+    window.open("./course-info/index-prog.html");
+}
+
+
